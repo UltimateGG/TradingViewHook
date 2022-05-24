dd0c64af827eb7c309ecc905e6c1d2cf4cac65f3
Gamer745 <clubblock1@gmail.com>
2022-05-24 14:21:59 -0500

    Initial setup and logging

---
+++ src/main/java/me/ultimate/tvhook/Main.java
@@ -0,0 +1,26 @@
+package me.ultimate.tvhook;
+
+import me.ultimate.tvhook.utils.Configuration;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    public static final boolean DEVELOPMENT_MODE = "dev".equals(System.getProperty("env"));
+    private static final Logger LOGGER = LogManager.getLogger("Main");
+    public static final String DATA_FOLDER = System.getProperty("user.dir")
+            + (DEVELOPMENT_MODE ? "\\src\\main\\resources\\" : "\\");
+    private static final Configuration CONFIG = new Configuration("config.yml");
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public static Logger getLogger() {
+        return LOGGER;
+    }
+
+    public static Configuration getConfig() {
+        return CONFIG;
+    }
+}
---
+++ src/main/java/me/ultimate/tvhook/utils/Configuration.java
@@ -0,0 +1,95 @@
+package me.ultimate.tvhook.utils;
+
+import me.ultimate.tvhook.Main;
+import org.yaml.snakeyaml.Yaml;
+
+import java.awt.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Configuration {
+    private Map<String, Object> configMap;
+    private boolean exists = false;
+
+
+    public Configuration(String fileName) {
+        this(new File(Main.DATA_FOLDER, fileName));
+    }
+
+    public Configuration(File file) {
+        try {
+            exists = file.exists();
+            if (!exists) {
+                Main.getLogger().warn("Configuration file not found: {}", file.getAbsolutePath());
+                configMap = new HashMap<>();
+                return;
+            }
+
+            Yaml yaml = new Yaml();
+            configMap = yaml.load(new FileInputStream(file));
+        } catch (Exception e) {
+            Main.getLogger().warn("Failed to load/parse configuration file: {}", file.getAbsolutePath());
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public Object get(String path) {
+        String[] keys = path.split("\\.");
+
+        Map<String, Object> map = configMap;
+        Object obj = null;
+        for (String key : keys) {
+            if (map.containsKey(key)) {
+                if (map.get(key) instanceof Map) {
+                    map = (Map<String, Object>) map.get(key);
+                } else {
+                    obj = map.get(key);
+                }
+            }
+        }
+
+        if (obj == null) Main.getLogger().warn("Unknown configuration key: {}", path);
+        return obj;
+    }
+
+    public String getString(String path) {
+        Object obj = get(path);
+        return (obj instanceof String) ? (String) obj : null;
+    }
+
+    public int getInt(String path, int def) {
+        Object obj = get(path);
+        return (obj instanceof Integer) ? (int) obj : def;
+    }
+
+    public double getDouble(String path, double def) {
+        Object obj = get(path);
+        return (obj instanceof Double) ? (double) obj : def;
+    }
+
+    public boolean getBoolean(String path, boolean def) {
+        Object obj = get(path);
+        return (obj instanceof Boolean) ? (boolean) obj : def;
+    }
+
+    // Java
+    public Color getColor(String path, Color def) {
+        int col = getInt(path, def.getRGB());
+        return new Color(col);
+    }
+
+    @SuppressWarnings("unchecked")
+    public ArrayList<String> getStringList(String path) {
+        Object obj = get(path);
+
+        if (obj instanceof ArrayList) return (ArrayList<String>) obj;
+        return new ArrayList<>();
+    }
+
+    public boolean exists() {
+        return exists;
+    }
+}
